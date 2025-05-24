c3729bfebee982c69c5bf284078b4c49cf4ca7be
Aroniii <aronaro911@gmail.com>
2025-05-24 21:32:43 +0100

    modified:   astro.config.mjs 	deleted:    src/assets/Purple 3D Icons - Copy@1-1876x974.jpg 	new file:   src/components/Spline3D.jsx 	modified:   src/pages/index.astro

---
+++ src/components/Spline3D.jsx
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Spline3D() {
+  return (
+    <main style={{ width: '100vw', height: '100vh', margin: 0 }}>
+      <Spline scene="https://prod.spline.design/d2ajuLLrFZIx9Eps/scene.splinecode" />
+    </main>
+  );
+}
